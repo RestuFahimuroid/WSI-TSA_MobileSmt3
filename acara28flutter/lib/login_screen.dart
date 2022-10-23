b372b4602621e4587174defed3909e77a6d78ac7
Restu <fahimmuroid@gmail.com>
2022-10-23 19:03:13 +0700

    first commit

--- acara28flutter/lib/login_screen.dart
+++ acara28flutter/lib/login_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:acara28flutter/signup_screen.dart';
 
--- acara28flutter/lib/splash_screen.dart
+++ acara28flutter/lib/splash_screen.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-
 import 'login_screen.dart';
 
 void main() {
